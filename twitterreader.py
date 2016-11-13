83a6cfd39cc4071629f42fd4008439b2cd70ba0d
Sohan Subhash <Sohan@airbears2-10-142-34-161.airbears2.1918.berkeley.edu>
2016-11-12 17:04:33 -0800

    reorganized code

--- twitterreader.py
+++ twitterreader.py
@@ -1,31 +1,48 @@
 import tweepy
+import json
 from tweepy import OAuthHandler
+from tweepy import Cursor
  
-consumer_key = 'ZZHlgZYoCR72HWvqZ4LbFMNcb'
-consumer_secret = 'Gx2g9Z5DwaBI3Bq0JyxK5pAwwPWqSdN4t1rf5mhghEhwMXGjzm'
 # access_token = '3320969365-Epm7HFGVOuPYKScLJYOpT9sMJMJHl1NvpNEnhn0'
 # access_secret = '7saqiEs5O6slpaqdXbJEZ9BAtRcI3HVceqWTUAEKslcRc'
- 
-auth = OAuthHandler(consumer_key, consumer_secret)
-try:
-    redirect_url = auth.get_authorization_url()
-except tweepy.TweepError:
-    print('Error! Failed to get request token.')
 
-print(redirect_url)
-verifier = input('Verifier:')
-try:
-    auth.get_access_token(verifier)
-except tweepy.TweepError:
-    print('Error! Failed to get access token')
+def get_twitter_auth():
+	# Setup Twitter autentication
+	consumer_key = 'ZZHlgZYoCR72HWvqZ4LbFMNcb'
+	consumer_secret = 'Gx2g9Z5DwaBI3Bq0JyxK5pAwwPWqSdN4t1rf5mhghEhwMXGjzm'
+	auth = OAuthHandler(consumer_key, consumer_secret)
+	try:
+		redirect_url = auth.get_authorization_url()
+	except tweepy.TweepError:
+		print('Error! Failed to get request token.')
 
-key = auth.access_token
-secret = auth.access_token_secret
-auth.set_access_token(key, secret)
- 
-api = tweepy.API(auth)
-api.update_status('Cal Hacks is dope as fuck')
+	print(redirect_url)
+	verifier = input('Verifier:')
 
+	try:
+		auth.get_access_token(verifier)
+	except tweepy.TweepError:
+		print('Error! Failed to get access token')
+
+	key = auth.access_token
+	secret = auth.access_token_secret
+	auth.set_access_token(key, secret)
+	return auth
+
+def get_twitter_client():
+	auth = get_twitter_auth() 
+	client = tweepy.API(auth) 
+	return client
+
+def process_timeline(tweet):
+	print(json.dumps(tweet))
+
+def get_timeline():
+	client = get_twitter_client() 
+	for tweet in tweepy.Cursor(api.user_timeline).items():
+		process_timeline(tweet._json)
+
+get_timeline()
 # api.update_status('@RuesgaSkyler tweepy + oauth!')
 
 # public_tweets = api.home_timeline()
