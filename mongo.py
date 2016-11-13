a007d0f29c9c54e70fdb3cee71a6ae3bd44fadf6
Skyler Ruesga <sruesga7323@gmail.com>
2016-11-12 20:15:18 -0800

    documented mongo.py

--- mongo.py
+++ mongo.py
@@ -8,20 +8,23 @@ connection = MongoClient()
 #initialize and assign collection
 db = connection.data
 
+#dump all tweets into db.tweets collection
 def process_tweet(tweet):
 	db.tweets.insert_many(json.dumps(tweet))
 
-
+#sorts tweets from oldest to newest
 timeline = db.tweets.find({}).sort({created_at: 1}) 
-
 
 #obtain one document where its an original tweet
 user = db.tweets.findOne({retweet: false})["author"]
 
+#get all tweets that aren't quoted or retweeted
 tweets = db.tweets.find({is_quote_status: false, retweeted: false, })
 
+#gets all retweeted tweets
 retweets = db.tweets.find({retweeted: true})
 
+#creates a dictionary of tweet id strings as keys and number of favorites as values
 fav_dict = {}
 def favorites(tweet):
 	for t in tweet:
