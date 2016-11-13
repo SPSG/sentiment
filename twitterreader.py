139177db4f4d04e684232cf03aa13afea4fcaf28
Skyler Ruesga <sruesga7323@gmail.com>
2016-11-12 23:17:52 -0800

    finalized mongo stuff

--- twitterreader.py
+++ twitterreader.py
@@ -3,6 +3,7 @@ import json
 from tweepy import OAuthHandler
 from tweepy import Cursor
 from pymongo import MongoClient
+from pprint import pprint
  
 # access_token = '3320969365-Epm7HFGVOuPYKScLJYOpT9sMJMJHl1NvpNEnhn0'
 # access_secret = '7saqiEs5O6slpaqdXbJEZ9BAtRcI3HVceqWTUAEKslcRc'
@@ -12,34 +13,6 @@ connection = MongoClient()
 
 #initialize and assign collection
 db = connection.data
-
-#dump all tweets into db.tweets collection
-def process_tweet(tweet):
-	db.tweets.insert(tweet)
-
-	#sorts tweets from oldest to newest
-	#timeline = db.tweets.find().sort({'created_at': 1})
-
-	#obtain one document where its an original tweet
-	user_cursor = db.tweets.find({'retweet': False})
-
-	#get all tweets that aren't quoted or retweeted
-	tweets_cursor = db.tweets.find({'is_quote_status': False, 'retweeted': False, })
-
-	#gets all retweeted tweets
-	retweets_cursor = db.tweets.find({'retweeted': True})
-
-	#creates a dictionary of tweet id strings as keys and number of favorites as values
-	#fav_dict = {}
-	#def favorites(tweet):
-	#	for t in tweet:
-	#		fav_dict[t["id_str"]] = t["favorite_count"]
-
-	#favorites(timeline)
-
-	for user in user_cursor:
-		print(user)
-
 
 def get_twitter_auth():
 	# Setup Twitter autentication
@@ -81,8 +54,39 @@ def get_user_timeline():
 	# change the line below to reflect whether testing or not
 	client = get_twitter_client(get_twitter_bot()) 
 	for tweet in tweepy.Cursor(client.user_timeline).items():
-		process_tweet(tweet._json)
+		db.tweets.insert(tweet._json)
+
 
 get_user_timeline()
 
-#db.tweets.drop()
+#sorts tweets from oldest to newest
+timeline = db.tweets.find({'retweeted': False}).sort('created_at', 1)
+
+#obtain one document where its an original tweet
+user_cursor = db.tweets.find_one({'retweeted': False})
+#if user_cursor:
+#	user_cursor = user_cursor['user']['name']
+
+#get all tweets that aren't quoted or retweeted
+tweets_cursor = db.tweets.find({'is_quote_status': False, 'retweeted': False, })
+
+#gets all retweeted tweets
+retweets_cursor = db.tweets.find({'retweeted': True})
+
+#
+
+#creates a dictionary of tweet id strings as keys and number of favorites as values
+fav_dict = {}
+def favorites(tweet):
+	for t in tweet:
+		fav_dict[t["id_str"]] = t["favorite_count"]
+	fav_dict['total_favs'] = sum(fav_dict.values())
+	fav_dict['total_posts'] = len(fav_dict) - 1
+	fav_dict['fav_ratio'] = fav_dict['total_favs'] / fav_dict['total_posts']
+favorites(timeline)
+
+pprint(user_cursor)
+
+
+db.tweets.drop()
+connection.close()
